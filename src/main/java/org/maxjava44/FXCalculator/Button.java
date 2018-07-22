4a7198d1f5ef77ab935fe835ea730ba8cb04231d
maximumdeveloping <max.gilsoul@live.com>
2018-07-22 22:17:01 +0200

    Update Button.java

--- src/main/java/org/maxjava44/FXCalculator/Button.java
+++ src/main/java/org/maxjava44/FXCalculator/Button.java
@@ -11,7 +11,7 @@
     with this program; if not, write to the Free Software Foundation, Inc.,
     51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.*/
 
-package application;
+package FXCalculator;
 
 public class Button extends javafx.scene.control.Button {
 
